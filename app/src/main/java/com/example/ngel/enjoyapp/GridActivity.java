732cf559dc9c1ee971ba1fadd63357f17b0a267a
Angelotas <angel.martin.bartolome.95@gmail.com>
2016-12-19 00:58:43 +0100

    Preferenciascompartidas de prueba

--- app/src/main/java/com/example/ngel/enjoyapp/GridActivity.java
+++ app/src/main/java/com/example/ngel/enjoyapp/GridActivity.java
@@ -1,7 +1,10 @@
 package com.example.ngel.enjoyapp;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class GridActivity extends Activity {
 
@@ -10,5 +13,21 @@ public class GridActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_grid);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_settings:
+                startActivity(new Intent(this, SettingsActivity.class));
+                return true;
+            default:
+                return false;
+        }
     }
 }
