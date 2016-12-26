f3bae401ce9e44e052eb7dc3d87689cff94aab2a
Angelotas <angel.martin.bartolome.95@gmail.com>
2016-12-26 17:57:12 +0100

    Acabado

--- app/src/main/java/com/example/ngel/enjoyapp/GridActivity.java
+++ app/src/main/java/com/example/ngel/enjoyapp/GridActivity.java
@@ -13,6 +13,7 @@ public class GridActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_grid);
+        this.onBackPressed();
     }
 
     @Override
